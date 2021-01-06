35d712ab77b13874923bf72f0069ed15c7b5d360
ziimiin14 <erzimin@gmail.com>
2021-01-06 19:19:32 +0800

    minor modify

--- Week3_2/tf_test_efficientnet.py
+++ Week3_2/tf_test_efficientnet.py
@@ -3,31 +3,32 @@ import numpy as np
 import matplotlib.pyplot as plt
 from tensorflow.keras.preprocessing.image import ImageDataGenerator as IDG
 
-pre_trained_model = tf.keras.applications.inception_v3.InceptionV3(input_shape=(150,150,3),include_top=False,
-                                                                   weights=None)
+pre_trained_model = tf.keras.applications.EfficientNetB0(input_shape=(150,150,3),include_top=False,
+                                                        weights='None')
 pre_trained_model.load_weights('inception_v3_weights_tf_dim_ordering_tf_kernels_notop.h5')
-
-# pre_trained_model.trainable=False
-for layer in pre_trained_model.layers:
-    layer.trainable=False
-
+pre_trained_model.trainable=False
 # pre_trained_model.summary()
+# print(len(pre_trained_model.non_trainable_weights))
 
-last_layer = pre_trained_model.get_layer('mixed10')
-last_output = last_layer.output
-print(last_output)
-
-x = tf.keras.layers.Flatten()(last_output)
+# inputs = tf.keras.Input(shape=(150,150,3))
+# x = pre_trained_model(inputs,training=False)
+# print('input = ', inputs)
+x = tf.keras.layers.GlobalAveragePooling2D()(pre_trained_model.output)
+# x = tf.keras.layers.BatchNormalization()(x)
+# x = tf.keras.layers.Flatten()(pre_trained_model.output)
 x = tf.keras.layers.Dense(1024,activation='relu')(x)
 x = tf.keras.layers.Dropout(0.2)(x)
-x = tf.keras.layers.Dense(1,activation='sigmoid')(x)
+outputs = tf.keras.layers.Dense(1,activation='sigmoid')(x)
 
-print(pre_trained_model.input)
-model = tf.keras.Model(pre_trained_model.input,x)
+model = tf.keras.Model(pre_trained_model.input,outputs)
 
-# model.summary()
 
-model.compile(loss='binary_crossentropy',optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
+# print(pre_trained_model.input)
+# model = tf.keras.Model(pre_trained_model.input,x)
+
+model.summary()
+
+model.compile(loss='binary_crossentropy',optimizer=tf.keras.optimizers.Adam(lr=0.001),
               metrics=['accuracy'])
 
 # Define training and validation directory
@@ -36,11 +37,11 @@ val_dir = '../Week1_2/dog_cat_partial_dataset/cats_and_dogs_filtered/validation'
 
 train_datagen = IDG(rescale=1/255.0,rotation_range=30,width_shift_range=0.2,height_shift_range=0.2,
                     shear_range=0.2,zoom_range=0.2,horizontal_flip=True,fill_mode='nearest')
+
 val_datagen = IDG(rescale=1/255.0)
 
 train_generator = train_datagen.flow_from_directory(train_dir,target_size=(150,150),batch_size=20,class_mode='binary')
 val_generator = val_datagen.flow_from_directory(val_dir,target_size=(150,150),batch_size=20,class_mode='binary')
-
 # callback = tf.keras.callbacks.TensorBoard(log_dir='log')
 
 
--- Week3_2/transfer_learning_cat_dog_test.py
+++ Week3_2/transfer_learning_cat_dog_test.py
@@ -3,18 +3,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 from tensorflow.keras.preprocessing.image import ImageDataGenerator as IDG
 
-pre_trained_model = tf.keras.applications.MobileNetV3Small(input_shape=(150,150,3),include_top=False,
+pre_trained_model = tf.keras.applications.MobileNetV2(input_shape=(150,150,3),include_top=False,
                                                         weights='imagenet')
 # pre_trained_model.load_weights('inception_v3_weights_tf_dim_ordering_tf_kernels_notop.h5')
 pre_trained_model.trainable=False
-pre_trained_model.summary()
-print(len(pre_trained_model.non_trainable_weights))
+# pre_trained_model.summary()
+# print(len(pre_trained_model.non_trainable_weights))
 
 model = tf.keras.models.Sequential([pre_trained_model,
                                     # tf.keras.layers.Flatten(),
                                     tf.keras.layers.GlobalAveragePooling2D(),
-                                    tf.keras.layers.BatchNormalization(),
-                                    # tf.keras.layers.Dense(512,activation='relu'),
+                                    # tf.keras.layers.BatchNormalization(),
+                                    tf.keras.layers.Dense(512,activation='relu'),
                                     tf.keras.layers.Dropout(0.2),
                                     tf.keras.layers.Dense(1,activation='sigmoid')])
 
@@ -32,11 +32,11 @@ val_dir = '../Week1_2/dog_cat_train_test/testing'
 
 train_datagen = IDG(rescale=1/255.0,rotation_range=30,width_shift_range=0.2,height_shift_range=0.2,
                     shear_range=0.2,zoom_range=0.2,horizontal_flip=True,fill_mode='nearest')
+
 val_datagen = IDG(rescale=1/255.0)
 
 train_generator = train_datagen.flow_from_directory(train_dir,target_size=(150,150),batch_size=20,class_mode='binary')
 val_generator = val_datagen.flow_from_directory(val_dir,target_size=(150,150),batch_size=20,class_mode='binary')
-
 # callback = tf.keras.callbacks.TensorBoard(log_dir='log')
 
 
